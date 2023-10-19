f2a0eed037c29fb87d4b51dc916fbdfe40bfaa31
iCastro117 <116144684+iCastro117@users.noreply.github.com>
2023-10-18 22:17:33 -0500

    Producto.java

--- Producto.java
+++ Producto.java
@@ -1,9 +1,42 @@
-import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
-public record Producto(String nombre, double precio, int codigo) {
+import java.util.List;
+import java.util.Scanner;
+class Producto {
+    private String nombre;
+    private double precio;
+    private int codigo;
+
+    public Producto(String nombre, double precio, int codigo) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.codigo = codigo;
+    }
+
     public String toString() {
-        return "Producto: " + nombre + "\nPrecio: $" + precio + "\nCodigo: " + codigo;
+        return "Producto: " + nombre + "\nPrecio: $" + precio + "\nCódigo: " + codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 }
-
