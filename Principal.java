49ffdd395b2f8604a5426b3f6392c068d2d1223a
iCastro117 <116144684+iCastro117@users.noreply.github.com>
2023-10-18 22:27:59 -0500

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,64 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Producto> productos = new ArrayList<>();
-
-        System.out.print("Ingrese la cantidad de productos: ");
-        int cantidadProductos = obtenerCodigoNice(scanner);
-
-        for (int i = 0; i < cantidadProductos; i++) {
-            scanner.nextLine();
-
-            System.out.println("Producto #" + (i + 1));
-            System.out.print("Nombre del producto: ");
-            String nombre = scanner.nextLine();
-
-            System.out.print("Precio del producto: ");
-            double precio = obtenerPrecioNice(scanner);
-
-            System.out.print("Código del producto: ");
-            int codigo = obtenerCodigoNice(scanner);
-
-            Producto producto = new Producto(nombre, precio, codigo);
-            productos.add(producto);
-        }
-
         Tienda tienda = new Tienda(productos);
-        tienda.procesarCompra(scanner);
-    }
 
-    private static int obtenerCodigoNice(Scanner scanner) {
         while (true) {
-            try {
-                int numero = scanner.nextInt();
-                if (numero > 0) {
-                    return numero;
-                } else {
-                    System.out.println("Por favor, ingrese un número entero positivo.");
-                }
-            } catch (Exception e) {
-                System.out.println("Por favor, ingrese un número entero positivo válido.");
-                scanner.nextLine();
-            }
-        }
-    }
+            System.out.println("Menú Principal");
+            System.out.println("1. Registrar Producto");
+            System.out.println("2. Procesar Compra");
+            System.out.println("3. Calcular Precio Total");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
 
-    private static double obtenerPrecioNice(Scanner scanner) {
-        while (true) {
-            try {
-                double numero = scanner.nextDouble();
-                if (numero >= 0) {
-                    return numero;
-                } else {
-                    System.out.println("Por favor, ingrese un número decimal no negativo.");
-                }
-            } catch (Exception e) {
-                System.out.println("Por favor, ingrese un número decimal válido.");
-                scanner.nextLine();
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    tienda.registrarProducto(scanner);
+                    break;
+                case 2:
+                    tienda.procesarCompra(scanner);
+                    break;
+                case 3:
+                    double total = tienda.calcularPrecioTotal();
+                    System.out.println("Precio total de la compra: $" + total);
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa. ¡Hasta luego!");
+                    System.exit(0);
+                default:
+                    System.out.println("Opción no válida.");
             }
         }
     }
