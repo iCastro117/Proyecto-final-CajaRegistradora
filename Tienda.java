f7f29077e58fce08c77aab55db933613d632c76e
iCastro117 <116144684+iCastro117@users.noreply.github.com>
2023-10-18 22:28:52 -0500

    Update Tienda.java

--- Tienda.java
+++ Tienda.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
+import java.util.List;
+import java.util.ArrayList;
 class Tienda {
     private List<Producto> productos;
 
@@ -9,48 +8,59 @@ class Tienda {
         this.productos = productos;
     }
 
+    public void registrarProducto(Scanner scanner) {
+        scanner.nextLine();
+        System.out.print("Nombre del producto: ");
+        String nombre = scanner.nextLine();
+        System.out.print("Precio del producto: ");
+        double precio = scanner.nextDouble();
+        System.out.print("Código del producto: ");
+        int codigo = scanner.nextInt();
+
+        Producto producto = new Producto(nombre, precio, codigo);
+        productos.add(producto);
+        System.out.println("Producto registrado con éxito.");
+    }
+
     public void procesarCompra(Scanner scanner) {
         double total = 0;
         System.out.println("Recibo de compra:");
-
         for (Producto producto : productos) {
             System.out.println(producto);
-
-            System.out.println("¿El producto " + producto.getNombre() + " tiene descuento?");
-            System.out.println("(1) Sí   (2) No   (3) Aplicar descuento personalizado:");
-
+            System.out.print("¿El producto " + producto.getNombre() + " tiene descuento? (1) Sí   (2) No: ");
             int opcion = scanner.nextInt();
             double precioFinal = 0;
 
             switch (opcion) {
                 case 1:
                     System.out.print("Ingrese el porcentaje de descuento (%): ");
-                    double porcentajeDescuento = scanner.nextDouble(); // Lee el porcentaje directamente
+                    double porcentajeDescuento = scanner.nextDouble();
                     double descuento = producto.getPrecio() * (porcentajeDescuento / 100);
                     precioFinal = producto.getPrecio() - descuento;
                     producto.setPrecio(precioFinal);
                     System.out.println("Descuento del " + porcentajeDescuento + "% aplicado a " + producto.getNombre());
                     break;
                 case 2:
-                    System.out.println("No se aplica ningún descuento");
+                    System.out.println("No se aplica ningún descuento.");
                     precioFinal = producto.getPrecio();
                     break;
-                case 3:
-                    System.out.print("Ingrese el descuento personalizado (%): ");
-                    double descuentoPersonalizado = scanner.nextDouble(); // Lee el descuento personalizado
-                    double descuentoPersonalizadoFinal = producto.getPrecio() * (descuentoPersonalizado / 100);
-                    precioFinal = producto.getPrecio() - descuentoPersonalizadoFinal;
-                    producto.setPrecio(precioFinal);
-                    System.out.println("Descuento personalizado de %" + descuentoPersonalizado + " aplicado a " + producto.getNombre());
-                    break;
                 default:
-                    System.out.println("Opción no válida");
+                    System.out.println("Opción no válida. No se aplica descuento.");
+                    precioFinal = producto.getPrecio();
+                    break;
             }
 
             total += precioFinal;
         }
 
-        System.out.println(" ");
         System.out.println("Total de la compra: $" + total);
+    }
+
+    public double calcularPrecioTotal() {
+        double total = 0;
+        for (Producto producto : productos) {
+            total += producto.getPrecio();
+        }
+        return total;
     }
 }
