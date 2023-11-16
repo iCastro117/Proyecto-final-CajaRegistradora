4fdaa44b058ca4dbed0373741b82e79835de0331
michelle2005Romero <142345226+michelle2005Romero@users.noreply.github.com>
2023-11-16 09:36:57 -0500

    Update Tienda.java

--- Tienda.java
+++ Tienda.java
@@ -1,5 +1,44 @@
 import java.util.List;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Producto> productos = new ArrayList<>();
+        Tienda tienda = new Tienda(productos);
+
+        while (true) {
+            System.out.println("Menú Principal");
+            System.out.println("1. Registrar Producto");
+            System.out.println("2. Procesar Compra");
+            System.out.println("3. Calcular Precio Total");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    tienda.registrarProducto(scanner);
+                    break;
+                case 2:
+                    tienda.procesarCompra(scanner);
+                    break;
+                case 3:
+                    double total = tienda.calcularPrecioTotal();
+                    System.out.println("Precio total de la compra: $" + total);
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa. ¡Hasta luego!");
+                    System.exit(0);
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        }
+    }
+}
 
 public class Tienda {
     private List<Producto> productos;
